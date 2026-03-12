7f6fcca48ae53d6e36d3f16a7568be7563dcc7a2
mathi <bb@geeksynergy.com>
2026-03-13 03:08:26 +0530

    gallery layout added

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,26 +1,56 @@
 package com.example.drawingapp
 
 
+import android.Manifest
 import android.app.Dialog
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private val cameraResultLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+
+            if (isGranted) {
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show()
+
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
+            }
+
+        }
+    private val cameraMultipleResultLauncher: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { isGranted ->
+
+           isGranted.forEach {
+               if(it.key== Manifest.permission.ACCESS_FINE_LOCATION){
+
+               }
+           }
+
+        }
     private var drawingView: DrawingView? = null
     private var brushButton: ImageButton? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         val linearLayoutColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
         mImageButtonCurrentPaint = linearLayoutColors[1] as ImageButton
         drawingView = findViewById(R.id.drawing_view)
@@ -31,6 +61,17 @@ class MainActivity : AppCompatActivity() {
         )
         brushButton!!.setOnClickListener {
             showBrushSizeChooseDialog()
+        }
+        val buttonCamera: ImageButton = findViewById(R.id.gallery)
+        buttonCamera.setOnClickListener {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                showRationalDialog(
+                    "Permission demo requires camera access",
+                    "Camera cannot be used because camera access is denied"
+                )
+            } else {
+                cameraResultLauncher.launch(Manifest.permission.CAMERA)
+            }
         }
 
     }
@@ -75,8 +116,19 @@ class MainActivity : AppCompatActivity() {
                     R.drawable.pallet_normal
                 )
             )
-            mImageButtonCurrentPaint=view
+            mImageButtonCurrentPaint = view
 
         }
+    }
+
+    private fun showRationalDialog(
+        title: String,
+        message: String
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title).setMessage(message).setPositiveButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
     }
 }
